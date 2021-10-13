283f154682bc3e304dc3fbd7363fbd3057969bb7
jhoandabit <92416625+jhoandabit@users.noreply.github.com>
2021-10-12 22:12:06 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    led.setBrightness(100)
+})
+input.onButtonPressed(Button.B, function () {
+    led.setBrightness(255)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
